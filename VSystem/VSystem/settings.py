0954803b44d6e4bf81ff4f85b0a71a294ad595eb
Felipe Castro <118694429+MarkZ1953@users.noreply.github.com>
2024-10-14 14:07:31 -0500

    Grades
    - Se eliminan las notas de los estudiantes por falta de tiempo

--- VSystem/VSystem/settings.py
+++ VSystem/VSystem/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'people',
     'courses',
     'enrollments',
-    'grades'
 ]
 
 MIDDLEWARE = [
--- VSystem/grades/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Grade
-
-
-@admin.register(Grade)
-class GradeAdmin(admin.ModelAdmin):
-    list_display = ("id", "course", "student", "grade")
